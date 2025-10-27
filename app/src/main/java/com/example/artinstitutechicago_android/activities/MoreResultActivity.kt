d26a58480bbeca2d272290bf975a36dcf2d2cadf
aaron <aaronelbarbaroii@gmail.com>
2025-10-27 13:37:16 +0100

    modify slider to have session value

--- app/src/main/java/com/example/artinstitutechicago_android/activities/MoreResultActivity.kt
+++ app/src/main/java/com/example/artinstitutechicago_android/activities/MoreResultActivity.kt
@@ -1,6 +1,8 @@
 package com.example.artinstitutechicago_android.activities
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -66,5 +68,18 @@ class MoreResultActivity : AppCompatActivity() {
             finish()
         }
 
+    }
+
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
